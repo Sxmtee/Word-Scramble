e3d8a55b8efb8732b1b32fe00d19fb4dd8928ee8
sxmtee <somtoo76@gmail.com>
2024-11-16 21:12:51 +0100

    Added Accessibility

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -141,6 +141,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
